22a90194e2f644749a4bcf0908543de818ee4ff5
Jerry Wu <qilong_wu@u.nus.edu>
2025-05-23 01:26:11 +0800

    Update mmk12.py & training script for 7B single turn grpo, Refactory to avoid conflict package name from source build.

---
+++ visagent/examples/data_preprocess/mmk12.py
@@ -0,0 +1,93 @@
+# Copyright 2025 Agent-X Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Preprocess the MMK12 dataset to parquet format
+
+huggingface-cli login
+PYTHONPATH=. python examples/data_preprocess/mmk12.py --local_dir data/mmk12 --n_test 707
+"""
+
+import argparse
+import os
+
+import datasets
+
+from verl.utils.hdfs_io import copy, makedirs
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_dir", default="data/mmk12")
+    parser.add_argument("--hdfs_dir", default=None)
+    parser.add_argument("--n_test", default=707)
+    args = parser.parse_args()
+
+    data_source = "Agents-X/MMK12"
+    os.makedirs(args.local_dir, exist_ok=True)
+    dataset = datasets.load_dataset(data_source, data_dir="data", split="train")
+    n = len(dataset)
+    n_train = n - int(args.n_test)
+
+    train_dataset = dataset.select(range(n_train))
+    test_dataset = dataset.select(range(n_train, n))
+
+    instruction_following = (
+        r"You FIRST think about the reasoning process as an internal monologue and then provide the final answer. "
+        r"The reasoning process MUST BE enclosed within <think> </think> tags. The final answer MUST BE put in \boxed{}."
+    )
+
+    # add a row to each data item that represents a unique id
+    def make_map_fn(split):
+        def process_fn(example, idx):
+            problem = "<image>" + example.pop("question")
+            prompt = problem + " " + instruction_following
+            answer = example.pop("correct_answer")
+            images = example.pop("image")
+            example.pop("message", None)
+            example.pop("predicted_answer", None)
+            example.pop("final_response", None)
+
+            data = {
+                "data_source": data_source,
+                "prompt": [
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    }
+                ],
+                "images": [images],
+                "ability": "math",
+                "reward_model": {"style": "rule", "ground_truth": answer},
+                "extra_info": {
+                    "split": split,
+                    "index": idx,
+                    "answer": answer,
+                    "question": problem,
+                },
+            }
+            return data
+
+        return process_fn
+
+    train_dataset = train_dataset.map(function=make_map_fn("train"), with_indices=True, num_proc=64)
+    test_dataset = test_dataset.map(function=make_map_fn("test"), with_indices=True, num_proc=64)
+
+    local_dir = args.local_dir
+    hdfs_dir = args.hdfs_dir
+
+    train_dataset.to_parquet(os.path.join(local_dir, "train.parquet"))
+    test_dataset.to_parquet(os.path.join(local_dir, "test.parquet"))
+
+    if hdfs_dir is not None:
+        makedirs(hdfs_dir)
+        copy(src=local_dir, dst=hdfs_dir)
